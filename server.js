68f038c08aa968545372f958e8c518e3089026e8
Aditya Sontakke <adityasontakke231205@gmail.com>
2026-09-03 17:09:59 +0530

    Update response message to version 2.0

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     console.log(`${req.method} ${req.url}`);
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from ECS Fargate! Version: 1.0\n');
+    res.end('Hello from ECS Fargate! Version: 2.0\n');
 });
 
 server.listen(3000, () => {
